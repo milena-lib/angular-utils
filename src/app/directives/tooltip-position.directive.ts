e093df8e1bfc0552fb18a281fa517f3027f7e16d
milena-lib <57179624+milena-lib@users.noreply.github.com>
2022-07-27 11:17:14 +0300

    Update tooltip-position.directive.ts

--- src/app/directives/tooltip-position.directive.ts
+++ src/app/directives/tooltip-position.directive.ts
@@ -13,6 +13,9 @@ export class TooltipPositionDirective {
 
     const height = Math.max( body.scrollHeight, body.offsetHeight, 
                        html.clientHeight, html.scrollHeight, html.offsetHeight );
+    
+    this.el.nativeElement.classList.remove('bottom'); 
+    this.el.nativeElement.classList.remove('top');
 
     if(event.clientY >= height - 50) {
       this.el.nativeElement.classList.add('top');
