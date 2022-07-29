82dbd3aa1cda89eb0d97b145b4e38874da083436
milena-lib <57179624+milena-lib@users.noreply.github.com>
2022-07-29 16:55:33 +0300

    storybook

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "stories": ['../src/app/components/**/*.stories.ts'],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/angular"
+}
--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -6,10 +6,46 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent implements OnInit {
-  @Input() caption: string;
   @Input() color: string;
   @Output() fnClick = new EventEmitter<string>();
  
+  /**
+   * Is this the principal call to action on the page?
+   */
+   @Input()
+   primary = false;
+ 
+   /**
+    * What background color to use
+    */
+   @Input()
+   backgroundColor?: string;
+ 
+   /**
+    * How large should the button be?
+    */
+   @Input()
+   size: 'small' | 'medium' | 'large' = 'medium';
+ 
+   /**
+    * Button contents
+    *
+    * @required
+    */
+   @Input()
+   label = 'Button';
+ 
+   /**
+    * Optional click handler
+    */
+  //  @Output() onClick = new EventEmitter<Event>();
+ 
+   public get classes(): string[] {
+     const mode = this.primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+ 
+     return ['storybook-button', `storybook-button--${this.size}`, mode];
+   }
+
   constructor() { }
 
   ngOnInit(): void {
