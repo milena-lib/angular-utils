e0439920bbd3ef9077dd03606a36e9bb4eec424e
milena-lib <57179624+milena-lib@users.noreply.github.com>
2022-08-01 10:11:50 +0300

    Update Button.stories.ts

--- src/app/components/button/Button.stories.ts
+++ src/app/components/button/Button.stories.ts
@@ -7,71 +7,80 @@ import { ButtonComponent } from './button.component';
 // More on default export: https://storybook.js.org/docs/angular/writing-stories/introduction#default-export
 export default {
   title: 'Example/Button',
-  component: ButtonComponent,
-  // More on argTypes: https://storybook.js.org/docs/angular/api/argtypes
-  argTypes: {
-    // Assigns the argTypes to the Colors category
-    backgroundColor: {
-      control: 'color',
-      table: {
-        category: 'Colors',
-        // Assigns the argTypes to a specific subcategory
-        subcategory: 'Button colors',
+    component: ButtonComponent,
+    // More on argTypes: https://storybook.js.org/docs/angular/api/argtypes
+    argTypes: {
+      // Assigns the argTypes to the Colors category
+      backgroundColor: {
+        control: 'color',
+        table: {
+          category: 'Colors',
+          // Assigns the argTypes to a specific subcategory
+          subcategory: 'Button colors',
+        },
       },
-    },
-    primary: {
-      table: {
-        category: 'Colors',
-        subcategory: 'Button style',
+      primary: {
+        table: {
+          category: 'Colors',
+          subcategory: 'Button style'
+        },
       },
-    },
-    label: {
-      table: {
-        category: 'Text',
-        subcategory: 'Button contents',
+      disabled: {
+        table: {
+            category: 'Colors',
+            subcategory: 'Button style',
+        }
       },
-    },
-    // Assigns the argType to the Events category
-    onClick: {
-      table: {
-        category: 'Events',
-        subcategory: 'Button Events',
+      label: {
+        table: {
+          category: 'Text',
+          subcategory: 'Button contents',
+        },
       },
-    },
-    // Assigns the argType to the Sizes category
-    size: {
-      table: {
-        category: 'Sizes',
+      // Assigns the argType to the Events category
+      onClick: {
+        table: {
+          category: 'Events',
+          subcategory: 'Button Events',
+        },
       },
+      // Assigns the argType to the Sizes category
+      size: {
+        table: {
+          category: 'Sizes',
+        },
+      },
+      
     },
-  },
-} as Meta;
+  } as Meta;
+  
+  // More on component templates: https://storybook.js.org/docs/angular/writing-stories/introduction#using-args
+  const Template: Story<ButtonComponent> = (args: ButtonComponent) => ({
+    props: args,
+  });
+  
+  export const Primary = Template.bind({});
+  // More on args: https://storybook.js.org/docs/angular/writing-stories/args
+  Primary.args = {
+    primary: true,
+    label: 'Click Me',
+    disabled: false,
+  };
+  
+  export const Secondary = Template.bind({});
+  Secondary.args = {
+    label: 'Button',
+    disabled: false,
+  };
 
-// More on component templates: https://storybook.js.org/docs/angular/writing-stories/introduction#using-args
-const Template: Story<ButtonComponent> = (args: ButtonComponent) => ({
-  props: args,
-});
-
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/angular/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: 'Click Me'
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
+  export const Large = Template.bind({});
+  Large.args = {
+    size: 'large',
+    label: 'Button',
+  };
+  
+  export const Small = Template.bind({});
+  Small.args = {
+    size: 'small',
+    label: 'Button',
+  };
